c0edf813066fc12d40efbca4965bf659ca093126
Anna-Maria Pfiel <anna-maria.pfiel@gmx.at>
2021-03-18 16:23:23 +0100

    d

--- main.js
+++ main.js
@@ -56,7 +56,7 @@ function outputData(person) {
   if ((new RegExp(/american/gi)).test(person.profession)) {
     row.classList.add('american');
   }
-  // Add a class to born_1920_1929
+  // Addd a class to born_1920_1929
   if ((new RegExp(/192/gi)).test(person.birthyear)) {
     row.classList.add('born_1920_1929');
   }
